f0194c7eaa8757dfaf4142fae48a98cd2da17e8c
TeachTalentStudio <teachtalentstudio@gmail.com>
2024-06-27 21:08:42 +0800

    add src folder

---
+++ src/script.js
@@ -0,0 +1,76 @@
+const topNav = [
+    {
+        logoSrc : "images/logo/Untitled-1.gif",
+        navLinks : ["Contact", "Barber", "Simple", "Learner", "simple"],
+    }   
+]
+
+const navBar = ["Learner","Gooder","Foostger","greater","bester"];
+
+const fullList = [
+    {
+        Contact : {
+            Bester : `
+             <img src='https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 387w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 687w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 774w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 987w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1287w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1374w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1587w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1887w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1974w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2187w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2487w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2574w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2787w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 3087w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 3174w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 3387w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 3687w, https://images.unsplash.com/photo-1719176372917-6c96c3608161?q=80&w=3759&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 3759w'/>   
+            `,
+            Gooder : []
+            
+        },
+        Learner : {
+            Onion: [],
+            ANimal: []
+
+        },
+        
+    },
+]
+
+
+
+
+
+const topNavLogo  = document.querySelector("#logo-image img");
+const topNavLinks = document.querySelector("#top-nav-links");
+const sidesNavLinks = document.querySelector("#side-nav-bar");
+const mainContent = document.querySelector("#main-content");
+
+
+
+
+
+topNavLogo.src = topNav[0].logoSrc;
+
+
+for (let TopNavKey in fullList[0]) {
+    // Create a new <a> element for each TopNavKey
+    let topNavAnchors = document.createElement("a");
+    topNavAnchors.innerHTML = TopNavKey;
+
+    // Append the <a> element to topNavLinks
+    topNavLinks.appendChild(topNavAnchors);
+
+    // Add click event listener to each <a> element
+    topNavAnchors.addEventListener('click', function() {
+        // Clear sidesNavLinks before adding new links
+        sidesNavLinks.innerHTML = '';
+
+        // Iterate over the keys of the object associated with the clicked TopNavKey
+        Object.keys(fullList[0][TopNavKey]).forEach(function(SideNavKey) {
+            let sideNavAnchors = document.createElement("a");
+            sideNavAnchors.innerHTML = SideNavKey;
+
+            // Append the <a> element to sidesNavLinks
+            sidesNavLinks.appendChild(sideNavAnchors);
+
+            // Add click event listener to each side navigation link
+            sideNavAnchors.addEventListener('click', function() {
+                // Clear contentP before displaying new content
+                mainContent.innerHTML = '';
+
+                // Display corresponding content in <p> tag based on SideNavKey
+                let content = fullList[0][TopNavKey][SideNavKey];
+                mainContent.innerHTML = content; // Assuming content is a string or HTML content
+            });
+        });
+    });
+}
